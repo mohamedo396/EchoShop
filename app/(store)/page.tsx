f3725b6e1df8d992240444303691846b05c99e30
mohamedo396 <mohamadamrani83@gmail.com>
2024-10-31 19:47:13 +0100

    first commit

---
+++ app/(store)/page.tsx
@@ -0,0 +1,10 @@
+
+import {Button} from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div >
+        <h1>Hello the fucking world motherfuckers</h1>
+        <Button>Fuck Me</Button>
+    </div>
+  );
+}
